f7f41811dd9394d7384929257a37ce94f01e273e
lhx <2539807128@qq.com>
2026-01-01 02:51:56 +0800

    更新前端axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "mern-think-board-backend11111.vercel.app/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://mern-think-board-backend11111.vercel.app/api";
 const api = axios.create({
     baseURL: BASE_URL,
 })
